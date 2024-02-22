4f914794d7c728852a538a9c1bae88655769dcde
Joey Boum Bletterman <jbbletterman@quintor.nl>
2024-02-22 13:32:56 +0100

    alternate method

--- tools/bcld-repo-manager/repository_manager.sh
+++ tools/bcld-repo-manager/repository_manager.sh
@@ -318,8 +318,17 @@ function apt_show () {
 
 # Function to scan for simple description
 function apt_show_description () {
-    count=$(( "${#PKG}" + 3 ))
     description="$(/usr/bin/apt-cache search --names-only "^${PKG}$")"
+    
+    if [[ -z "${description}" ]]; then
+        # If not found, use 'show' intead
+        count=16
+        description=$(/usr/bin/apt-cache show "${PKG}" | /usr/bin/grep -m1 'Description-en')
+    else
+        # If found, cut label and delimiter
+        count=$(( "${#PKG}" + 3 ))
+    fi
+    
     add_pkg_list "   Description:\t\"${description:$count}\""
 }
 
