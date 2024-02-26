0ff9c5c8ae5ac78a66f782c15192befd5df06780
Joey Boum Bletterman <jbbletterman@quintor.nl>
2024-02-26 12:21:57 +0100

    convert to markdown

--- test/SHELL-CHECK.sh
+++ test/SHELL-CHECK.sh
@@ -2,9 +2,9 @@
 # Script for ShellCheck automation
 
 if [[ -x /usr/bin/shellcheck ]] && [[ -f ./test/00_BCLD-BUILD.bats ]]; then
-    SHELL_REPORT='./test/SHELL-REPORT.txt'
+    SHELL_REPORT='./test/SHELL-REPORT.md'
     
-    /usr/bin/echo -e '\nStarting BCLD ShellCheck'
+    /usr/bin/echo -e '\n#Starting BCLD ShellCheck'
     
     # Make necessary directories
     /usr/bin/mkdir -p "$(/usr/bin/dirname ${SHELL_REPORT})"
@@ -21,7 +21,7 @@ if [[ -x /usr/bin/shellcheck ]] && [[ -f ./test/00_BCLD-BUILD.bats ]]; then
 
     ## If ShellCheck finds warnings...    
     if [[ ${SHELL_WARN} -gt 0 ]]; then
-        /usr/bin/echo '# Common warnings found:' | /usr/bin/tee -a "${SHELL_REPORT}"
+        /usr/bin/echo -e '\n## Common warnings found:' | /usr/bin/tee -a "${SHELL_REPORT}"
         
         COMMON_WARNINGS="$(/usr/bin/cat ${SHELL_REPORT} | /usr/bin/grep '(warning)' | /usr/bin/awk '{ print $2 }' | /usr/bin/sort -u)"
         
@@ -29,18 +29,18 @@ if [[ -x /usr/bin/shellcheck ]] && [[ -f ./test/00_BCLD-BUILD.bats ]]; then
             
             description="$(/usr/bin/cat "${SHELL_REPORT}" | /usr/bin/grep "${warn}" | /usr/bin/grep -v 'https' | /usr/bin/cut -d ':' -f2 | /usr/bin/sort -u)"
             
-            /usr/bin/echo "${warn}:${description}"
+            /usr/bin/echo " - ${warn}:${description}" | /usr/bin/tee -a "${SHELL_REPORT}"
         done 
     fi
     
     # ERRORS
     SHELL_ERROR="$(/usr/bin/cat "${SHELL_REPORT}" | /usr/bin/grep -c '(error)')"
     
-    /usr/bin/echo -e "\n# ShellCheck Errors: ${SHELL_ERROR}" | /usr/bin/tee -a "${SHELL_REPORT}"
+    /usr/bin/echo -e "\n# ShellCheck ERRORS: ${SHELL_ERROR}" | /usr/bin/tee -a "${SHELL_REPORT}"
     
     ## If ShellCheck finds errors...    
     if [[ ${SHELL_ERROR} -gt 0 ]]; then
-        /usr/bin/echo '# Common errors found:' | /usr/bin/tee -a "${SHELL_REPORT}"
+        /usr/bin/echo -e '\n## Common ERRORS found!:' | /usr/bin/tee -a "${SHELL_REPORT}"
         
         COMMON_ERRORS=$(/usr/bin/cat ${SHELL_REPORT} | /usr/bin/grep '(error)' | /usr/bin/awk '{ print $2 }' | /usr/bin/sort -u)
         
@@ -48,7 +48,7 @@ if [[ -x /usr/bin/shellcheck ]] && [[ -f ./test/00_BCLD-BUILD.bats ]]; then
             
             description="$(/usr/bin/cat "${SHELL_REPORT}" | /usr/bin/grep "${error}" | /usr/bin/grep -v 'https' | /usr/bin/cut -d ':' -f2 | /usr/bin/awk '{$1=$1};1')"
             
-            /usr/bin/echo "${error}: ${description}"
+            /usr/bin/echo " - ${error}: ${description}" | /usr/bin/tee -a "${SHELL_REPORT}"
         done
 
         exit 1    
