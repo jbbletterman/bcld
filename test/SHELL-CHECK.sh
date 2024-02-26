e309a59486ad959de93f199c93f65c6ee24cde3b
Joey Boum Bletterman <jbbletterman@quintor.nl>
2024-02-26 12:01:43 +0100

    Shell Check human readable

--- test/SHELL-CHECK.sh
+++ test/SHELL-CHECK.sh
@@ -14,19 +14,48 @@ if [[ -x /usr/bin/shellcheck ]] && [[ -f ./test/00_BCLD-BUILD.bats ]]; then
         -not \( -path './chroot' -o -path './modules' \) \
         -exec shellcheck -S warning {} \; > "${SHELL_REPORT}"
     
-    SHELL_ERROR="$(/usr/bin/cat "${SHELL_REPORT}" | /usr/bin/grep -c 'error')"
-    SHELL_WARN="$(/usr/bin/cat "${SHELL_REPORT}" | /usr/bin/grep -c 'warning')"
-    
-    /usr/bin/echo -e '\n' >> "${SHELL_REPORT}"
-    /usr/bin/echo -e "# ShellCheck Errors: ${SHELL_ERROR}\n" | /usr/bin/tee -a "${SHELL_REPORT}"
+    SHELL_ERROR="$(/usr/bin/cat "${SHELL_REPORT}" | /usr/bin/grep -c '(error)')"
+    SHELL_WARN="$(/usr/bin/cat "${SHELL_REPORT}" | /usr/bin/grep -c '(warning)')"
+
     /usr/bin/echo -e "# ShellCheck Warnings: ${SHELL_WARN}\n" | /usr/bin/tee -a "${SHELL_REPORT}"
-    /usr/bin/echo "# ShellCheck report: ${SHELL_REPORT}" | /usr/bin/tee -a "${SHELL_REPORT}"
-    
-    if [[ ${SHELL_ERROR} -gt 0 ]]; then
-        /usr/bin/echo -e '\n# ShellCheck found errors!' | /usr/bin/tee -a "${SHELL_REPORT}"
-        exit 1    
+
+
+    # If ShellCheck finds warnings...    
+    if [[ ${SHELL_WARN} -gt 0 ]]; then
+        /usr/bin/echo -e "\n# ShellCheck found warnings: ${SHELL_WARN}\n" | /usr/bin/tee -a "${SHELL_REPORT}"
+        /usr/bin/echo '# Common warnings found:' | /usr/bin/tee -a "${SHELL_REPORT}"
+        
+        COMMON_WARNINGS="$(/usr/bin/cat ${SHELL_REPORT} | /usr/bin/grep '(warning)' | /usr/bin/awk '{ print $2 }' | /usr/bin/sort -u)"
+        
+        for warn in ${COMMON_WARNINGS}; do
+            echo "$warn"
+        done
+            
+            description="$(/usr/bin/cat "${SHELL_REPORT}" | /usr/bin/grep "${warn}" | /usr/bin/grep -v 'https' | /usr/bin/cut -d ':' -f2 | /usr/bin/sort -u)"
+            
+            /usr/bin/echo "${warn}:${description}"
+        done 
     fi
     
+    # If ShellCheck finds errors...    
+    if [[ ${SHELL_ERROR} -gt 0 ]]; then
+        /usr/bin/echo -e "\n# ShellCheck found errors!: ${SHELL_ERROR}\n" | /usr/bin/tee -a "${SHELL_REPORT}"
+        /usr/bin/echo '# Common errors found:' | /usr/bin/tee -a "${SHELL_REPORT}"
+        
+        COMMON_ERRORS=$(/usr/bin/cat ${SHELL_REPORT} | /usr/bin/grep '(error)' | /usr/bin/awk '{ print $2 }' | /usr/bin/sort -u)
+        
+        for error in ${COMMON_ERRORS}; do
+            
+            description="$(/usr/bin/cat "${SHELL_REPORT}" | /usr/bin/grep "${error}" | /usr/bin/grep -v 'https' | /usr/bin/cut -d ':' -f2 | /usr/bin/awk '{$1=$1};1')"
+            
+            /usr/bin/echo "${error}: ${description}"
+        done
+
+        exit 1    
+    fi
+
+    /usr/bin/echo "# ShellCheck report: ${SHELL_REPORT}" | /usr/bin/tee -a "${SHELL_REPORT}"
+
 else
     /usr/bin/echo 'ShellCheck could not be found!'
     exit 1
