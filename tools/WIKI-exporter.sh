d7d16cc88376f680721e3cf68fa4b2317fae5fde
Joey Boum Bletterman <jbbletterman@quintor.nl>
2024-03-25 13:41:20 +0100

    wrong flag

--- tools/WIKI-exporter.sh
+++ tools/WIKI-exporter.sh
@@ -21,7 +21,7 @@ fi
 if [[ -d ./modules ]]; then
     cd ./modules
         list_entry
-        /usr/bin/zip -r -b "${ART_DIR}/bcld.wiki.zip" modules/bcld.wiki \
+        /usr/bin/zip -r "${ART_DIR}/bcld.wiki.zip" modules/bcld.wiki \
         && list_catch \
         && on_completion \
         || list_catch \
