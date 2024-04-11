ebfbeabf19371470467e1a59a213fffa23ddd828
Joey Boum Bletterman <jbbletterman@quintor.nl>
2024-04-11 12:38:30 +0200

    add subject info

--- test/bcld_test.sh
+++ test/bcld_test.sh
@@ -78,10 +78,18 @@ function BCLD_CMDs () {
 
 ## Function to display BCLD certificates
 function BCLD_CERTs () {
+    CA_HASH="$(/usr/bin/openssl x509 -in /etc/ssl/certs/ca.crt -noout -hash)"
+	CA_DATE="$(/usr/bin/openssl x509 -in /etc/ssl/certs/ca.crt -noout -enddate | /usr/bin/cut -d '=' -f2)"
+
     CERT_HASH="$(/usr/bin/openssl x509 -in /etc/ssl/certs/bcld.crt -noout -hash)"
 	CERT_DATE="$(/usr/bin/openssl x509 -in /etc/ssl/certs/bcld.crt -noout -enddate | /usr/bin/cut -d '=' -f2)"
 	
 	list_header 'Checking BCLD certificates'
+	list_item "CA certificate: ${CA_HASH}"
+	list_item "EXPIRES: ${CA_DATE}"
+	list_entry
+	/usr/bin/openssl x509 -in /etc/ssl/certs/ca.crt -noout -subject
+	list_catch
 	list_item "Client certificate: ${CERT_HASH}"
 	list_item "EXPIRES: ${CERT_DATE}"
 	list_entry
