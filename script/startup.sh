f7493db421c8c7b3acaeca65c99d134523cecdde
Joey Boum Bletterman <jbbletterman@quintor.nl>
2024-04-16 16:27:12 +0200

    network check fixes

--- script/startup.sh
+++ script/startup.sh
@@ -365,6 +365,13 @@ function ip_link () {
 			    # Only perform network check on BCLD_URL (trusted)
 			    list_item_pass "Performing network check on: \"${BCLD_URL}\""
 			    export BCLD_DOWNLOAD="$(/usr/bin/curl -s -o /dev/null -w '%{speed_download}' "${BCLD_URL}")"
+			    
+			    # If BCLD_URL is set, but network check fails, this network is unstable
+			    if [[ "${BCLD_DOWNLOAD}" -eq 0 ]]; then
+			        list_item_fail "Network check failed!"
+			        trap_shutdown 'net'
+			    fi
+			    
 			fi
 			
 			export BCLD_IP="$(/usr/sbin/ip address | /usr/bin/grep "${BCLD_IF}" | /usr/bin/grep inet | /usr/bin/awk '{ print $2 }' | /usr/bin/cut -d '/' -f1 | /usr/bin/head -n 1)"
@@ -620,7 +627,7 @@ function init_app () {
 	    	write_ENVs
 	    	
 	    	# Only TEST can escape the app and reset the terminal
-	    	launch
+	    	# launch
 	        reset_terminal
         else
             # If not TEST, launch the app normally but shutdown if it halts
