d9ee9cd80c28f9240e68cfd0049840b08f197dd6
Joey Boum Bletterman <jbbletterman@quintor.nl>
2025-04-25 16:17:00 +0200

    update chroot

--- script/chroot.sh
+++ script/chroot.sh
@@ -60,6 +60,8 @@ source "${BUILD_CONF}" \
 BCLD_ROOT='/root'
 BCLD_MOUNT='/media/BCLD-USB'
 CHROOT_RM='/opt/remotelogging'
+CHSURFACE_APT='/etc/apt/sources.list.d/linux-surface.list'
+CHSURFACE_KEY='/etc/apt/trusted.gpg.d/linux-surface.gpg'
 SSHD="/etc/ssh/sshd_config.d/10-BCLD.sh"
 SUDOERS="/etc/sudoers"
 
@@ -112,15 +114,17 @@ function set_passwd () {
 
 
 ### Install packages ###
-CHSURFACE_KEY="/etc/apt/trusted.gpg.d/linux-surface.gpg"
 
+# Linux Surface repo
 /usr/bin/curl -s https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
     | /usr/bin/gpg --dearmor | /usr/bin/dd of="${CHSURFACE_KEY}"
 
+/usr/bin/echo 'deb [arch=amd64] https://pkg.surfacelinux.com/debian release main' > "${CHSURFACE_APT}"
+
 list_item 'Checking Linux Surface GPG key...'
 if [[ -f ${CHSURFACE_KEY} ]] \
-    && [[  "$(/usr/bin/wc -l < ${CHSURFACE_KEY})" -gt 0 ]]; then
-    list_item_pass "Linux Surface GPG key found! $(/usr/bin/md5sum ${CHSURFACE_KEY} | awk '{ print $1 }')"
+    && [[  "$(/usr/bin/wc -l < "${CHSURFACE_KEY}")" -gt 0 ]]; then
+    list_item_pass "Linux Surface GPG key found! $(/usr/bin/md5sum "${CHSURFACE_KEY}" | /usr/bin/awk '{ print $1 }')"
     list_entry
     /usr/bin/gpg --list-keys --keyring "${CHSURFACE_KEY}" || exit 1
     /usr/bin/gpg --fingerprint --keyring "${CHSURFACE_KEY}" || exit 1
